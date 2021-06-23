9bce477bbce37a416f7a714ea9754905b1a0c91d
MadHatter0616 <56076176+MadHatter0616@users.noreply.github.com>
2021-06-23 23:00:55 +0800

    session 72 open door

---
+++ BuildingEsacpe/Source/BuildingEsacpe/OpenDoor.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Creating a Owner 
+	AActor* Owner = GetOwner();
+	//Creating new Rotator
+	FRotator NewRotator = FRotator(0.0f, 90.0f, 0.0f);
+	//Set new Rotator
+	Owner->SetActorRotation(NewRotator);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
